73d793ebcc4ba7a8035d9a85db69e15defb57ade
ffrescott <zenfany54@gmail.com>
2021-09-22 23:44:48 +0700

    masih belum selesai juga masih bingung retrofit, udah malam waktunya tidur

--- app/src/main/java/com/zen4r17/moviecatalog/MovieDetailActivity.kt
+++ app/src/main/java/com/zen4r17/moviecatalog/MovieDetailActivity.kt
@@ -2,12 +2,48 @@ package com.zen4r17.moviecatalog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.zen4r17.moviecatalog.models.MovieDetailResponse
+import kotlinx.android.synthetic.main.activity_movie_detail.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class MovieDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
 
+//        tv_set_title.text = intent.getStringExtra("TITLE")
+//        tv_overview.text = intent.getStringExtra("OVERVIEW")
 
+        val id: Int = intent.getIntExtra("ID", 0)
+        getDetailMovie(id)
+    }
+
+    private fun getDetailMovie(id: Int) {
+
+        RetrofitClient.instance.getDetailMovie(id)
+            .enqueue(object : Callback<MovieDetailResponse>) {
+
+                override fun onResponse(
+                    call: Call<MovieDetailResponse>?,
+                    response: Response<MovieDetailResponse>?
+
+                ) {
+                    if (response!!.isSuccessful) {
+                        tv_set_title = response.body().results
+                    }
+                }
+
+                override fun onFailure(call: Call<MovieDetailResponse>?, t: Throwable?) {
+
+                    Toast.makeText(this@MovieDetailActivity, "Gagal request", Toast.LENGTH_LONG)
+                        .show()
+                }
+
+
+            }
     }
 }
--- app/src/main/java/com/zen4r17/moviecatalog/services/MovieApiInterface.kt
+++ app/src/main/java/com/zen4r17/moviecatalog/services/MovieApiInterface.kt
@@ -1,11 +1,18 @@
 package com.zen4r17.moviecatalog.services
 
+import com.zen4r17.moviecatalog.models.MovieDetailResponse
 import com.zen4r17.moviecatalog.models.MovieResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
-interface MovieApiInterface  {
+interface MovieApiInterface {
 
     @GET("/3/movie/popular?api_key=a23df1152eff37dc83b5ef4e931534c4")
     fun getMovieList(): Call<MovieResponse>
+
+    @GET("movie/{movie_id}?api_key=a23df1152eff37dc83b5ef4e931534c4")
+    fun getDetailMovie(
+        @Path("movie_id") movie_id: Int
+    ): Call<MovieDetailResponse>
 }
